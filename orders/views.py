1d920d8a4ebf7dbdd4eaf85711aad5d3b8d48dd1
Deeksha Rao <11deesharao@gmail.com>
2025-12-23 11:57:40 +0530

    64435

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.framework.views import APIView
+from rest_framework.response import response
+from rest_framework import status
+from django.utils import timezone
+from .models import Coupon
 
-# Create your views here.
+class CouponValidationView(APIView):
+    def post(Self,request):
+        code=request.data.get('code')
+        if not code:
+            return Response(
+                {"error":"Coupon code is required"},
+                satus=status.HTTP_400_BAD_REQUEST
+            )
+        try:
+            coupon=Coupon.objects.get(code=code)
+        except Coupon.DoesNotExist:
+            return Response(
+            {"error":"Invalid coupon code"},
+            status=status.HTTP_400_BAD_REQUEST)
+        today=timezone.now().date()
+        if not coupon.is_active:
+            return Response(
+                {"error":coupon is inactive},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if not(coupon.valid_from<=today<=coupon.valid_until):
+            return Response(
+                {"error":"coupon is experied or not yet valid"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(
+            {
+                "success":True,
+                "discount_percentage":coupon.discount_percentage
+            },
+            status=status.HTTP_200_OK
+        )
+    
+
