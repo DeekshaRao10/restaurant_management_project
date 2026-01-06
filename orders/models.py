88f3b19f7576e5bdedae6a50efe9d9318e08a5d5
Deeksha Rao <11deesharao@gmail.com>
2026-01-06 20:09:54 +0530

    65389

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ class OrderStatus(models.Model):
     name=models.CharField(max_length=50,unique=True)
     def __str__(self):
         return self.name
-class order(models.Model):
+class Order(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     total_price=models.DecimalField(max_digits=10,decimal_places=2)
     status=models.ForeignKey(
