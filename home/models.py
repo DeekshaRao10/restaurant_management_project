6521098473d7a02729b1a0036bada091747a58b9
Deeksha Rao <11deesharao@gmail.com>
2026-01-06 19:56:09 +0530

    65387

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class Restaurant(models.Model):
+    name=models.CharField(max_length=255)
+    owner_name=models.CharField(max_length=255)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=20)
+    address=models.TextField()
+    city=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+    has_delivery=models.BooleanField(default=False)
+    def __str__(self):
+        return self.name
