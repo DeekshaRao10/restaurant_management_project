4cf4aa4edbc5db03531aabeff66acae44b778478
Deeksha Rao <11deesharao@gmail.com>
2026-01-12 20:14:42 +0530

    65503

--- orders/utils.py
+++ orders/utils.py
@@ -7,3 +7,8 @@ def generate_coupon_code(length=10):
         code=''.join(secrets.choice(characters) for _ in range(length))
         if not Coupon.objects.filter(code=code).exists():
             return code
+def get_daily_sales_total(Data):
+    result=Order.objects.filter(
+        created_at__date=created_at__date
+    ).aggregate(total_sum=sum('total_price'))
+    return result['total_sum'] or 0
