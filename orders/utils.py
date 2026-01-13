8ed44dd56b4c640f517ba212f765b0bef5b84511
Deeksha Rao <11deesharao@gmail.com>
2026-01-13 23:03:28 +0530

    65505

--- orders/utils.py
+++ orders/utils.py
@@ -1,8 +1,11 @@
 import secrets
 import string
+from django.db.models import sum
+from decimal import Decimal, ROUND_HALF_UP
+
 from .models import Coupon
 def generate_coupon_code(length=10):
-    characters=string.ascii_uppercase+string.digits
+    characters=string.ascii_uppercase + string.digits
     while True:
         code=''.join(secrets.choice(characters) for _ in range(length))
         if not Coupon.objects.filter(code=code).exists():
@@ -12,3 +15,8 @@ def get_daily_sales_total(Data):
         created_at__date=created_at__date
     ).aggregate(total_sum=sum('total_price'))
     return result['total_sum'] or 0
+def calculate_trip_amount(order_total, tip_opercentage):
+    order_total=Decimal(str(order_total))
+    tip_percentage=Decimal(str(tip_percentage))
+    tip_amount=order_total *(tip_percentage /Decimal("100"))
+    return tip_amount.quantize(Decimal("0.01"),rounding=ROUND_HALF_UP)
