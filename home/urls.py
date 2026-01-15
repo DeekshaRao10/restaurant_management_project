f38c25567a5810e8aed269b9b2e515c01c6d2005
Deeksha Rao <11deesharao@gmail.com>
2026-01-15 11:11:24 +0530

    65506

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
-from .views import *
+from django.urls import path,inlcude
+from rest_framework.routers import DefaultRouter
+from .views import MenuItemSearchViewSet
+
+router=DefaultRouter()
+router.register(r"menu-item/serach",MenuItemSearchViewSet,basename="menu-item-serach")
+
+urlpatterns=[
+    path("",include(router.urls)),
+]
 
 urlpatterns = [
     path('menu-categories/',MenuCategoryListView.as_view(),name='menu-categories'),
