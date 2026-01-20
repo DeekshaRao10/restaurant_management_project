7d443763d6abe400006288f68d9c44c9bfd1513f
Deeksha Rao <11deesharao@gmail.com>
2026-01-20 19:58:58 +0530

    65607

--- home/views.py
+++ home/views.py
@@ -1,6 +1,27 @@
+from rest_Framework import viewsets,status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
 from rest_framework.generics import ListAPIView
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
+
+class MenuItemUpdateViewSet(viewsets.ViewSet):
+    permission_classes=[IsAdminUser]
+
+    def update(Self,request,pk=None):
+        try:
+            menu_item=MenuItem.objects.get(pk=pk)
+        except MenuItem.DoesNotExist:
+            return Response(
+                {"error":"Menu item not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer=MenuItemSerializer(menu_item,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
 class MenuCategoryListView(ListAPIView):
     queryset=MenuCategory.objects.all()
     serializer_class=MenuCategorySerializer
