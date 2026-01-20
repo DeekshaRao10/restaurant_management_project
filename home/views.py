628644938717b6ac1594fb2851407009b186a8b3
Deeksha Rao <11deesharao@gmail.com>
2026-01-20 19:34:48 +0530

    65604

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from .serializers import MenuCategorySerializer
 class MenuCategoryListView(ListAPIView):
     queryset=MenuCategory.objects.all()
     serializer_class=MenuCategorySerializer
+    def retrieve(Self,request,*args,**kwargs):
+        menu_item=self.get_object()
+        ingredientts=menu_item.ingrediants.all()
+        serializer=IngredientSerializer(ingredients,many=True)
+        return Response(serializer.dat,status=status.HTTP_200_0K)
