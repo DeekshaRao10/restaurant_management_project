f4ac1577a11787c4a793f43d18cc216e6d0cb06f
Deeksha Rao <11deesharao@gmail.com>
2026-01-20 19:51:01 +0530

    65607

--- home/serializers.py
+++ home/serializers.py
@@ -8,3 +8,12 @@ class MenuCategorySerializer(Serializer.ModelSerializer):
     class Meta:
         model=Ingredient
         fields=['id','name']
+class MenuItemSerializer(Serializers.ModelSerializer):
+    class Meta:
+        model=MenuItem
+        fields='__all__'
+    def validate_price(self,value):
+        if value<=0:
+            raise serializers.ValidationError("price must be a positive number")
+        return value
+       
