795dcd459688bc79c876a4f286efe557063a8466
Deeksha Rao <11deesharao@gmail.com>
2026-01-20 19:25:44 +0530

    65603

--- home/utils.py
+++ home/utils.py
@@ -7,3 +7,14 @@ def get_today_operating_hours():
         return operating_hours.open_time,operating_hours.close_time
     except DailyOperatingHours.DoesNotExist:
         return None, None
+def is_restaurant_open():
+    now=datetime.now()
+    current_Day=now.weekday()
+    current_time=now time()
+    if current_day<5:
+        open_time=time(9,0)
+        close_time=time(22,0)
+    else:
+        open_time=time(10,0)
+        close_time=time(23,0)
+    return open_time<=current<=close_time
