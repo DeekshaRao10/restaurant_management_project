412590f30dab77a7bf3d109e2415408ad38e5f15
Deeksha Rao <11deesharao@gmail.com>
2026-01-25 15:55:11 +0530

    65763

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderStatus
 
-# Register your models here.
+def mark_orders_processed(modeladmin,request,queryset):
+    processed_status,_=OrderStatus.objects.get_or_create(name="processsed")
+    queryset.update(status=processed_status)
+mark_orders_processed.short_description="mark selected orders as processe"
+
+@admin.register(order)
+class OrderAdmin(admin.Admin):
+    list_display=("id","created_at","total_price","statsu")
+    list_Filter=("status","created_at")
+    actions=[mark_orders_processed]
